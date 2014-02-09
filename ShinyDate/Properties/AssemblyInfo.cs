31988bf9e6e8a1b4b95697247b300a5713603fc0
Richard Brown <richard.allan.brown@gmail.com>
2014-02-09 20:20:03 +0000

    Populating Assembly Info

--- ShinyDate/Properties/AssemblyInfo.cs
+++ ShinyDate/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ShinyDate")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple DateTime extention library for .NET")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ShinyDate")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Richard Brown 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
